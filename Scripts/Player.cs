58bb6e2e5148a4ba79b46a6080e1a934080411af
lara <l.leibetseder@students.htl-leonding.ac.at>
2026-02-06 17:34:39 +0100

    Implement laser shooting functionality and add laser prefab

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 namespace SpaceShooter.Scripts;
 
@@ -18,10 +19,20 @@ public sealed partial class Player : Sprite2D
 	[Export]
 	private float _minY = 400F;
 
+	[Export]
+	private PackedScene? _laserPrefab;
+
+	[Export]
+	private double _shotCooldownSeconds = 0.5;
+
 	private static readonly StringName moveUpAction = "move_up";
 	private static readonly StringName moveDownAction = "move_down";
 	private static readonly StringName moveLeftAction = "move_left";
 	private static readonly StringName moveRightAction = "move_right";
+
+	private static readonly StringName shootAction = "shoot";
+
+	private double _lastShotTime;
 
 	private PlayerBoundaries _boundaries;
 	// Called when the node enters the scene tree for the first time.
@@ -35,6 +46,7 @@ public sealed partial class Player : Sprite2D
 	public override void _Process(double delta)
 	{
 		Move(delta);
+		Shoot();
 	}
 
 	private void Move(double delta)
@@ -73,6 +85,31 @@ public sealed partial class Player : Sprite2D
 			Position = new Vector2(_boundaries.MinX, Position.Y);
 		}
 	}
+
+	private void Shoot()
+	{
+		if (!Input.IsActionPressed(shootAction))
+		{
+			return;
+		}
+
+		if (_laserPrefab is null)
+		{
+			throw new InvalidOperationException("Laser prefab is not set");
+		}
+
+		var now = Time.GetTicksMsec() / 1000.0;
+		if (now - _lastShotTime < _shotCooldownSeconds)
+		{
+			return;
+		}
+
+		var laser = _laserPrefab.Instantiate<Laser>();
+		laser.Position = Position + (Vector2.Up * 10F);
+		GetParent().AddChild(laser);
+		_lastShotTime = now;
+	}
+
 	private PlayerBoundaries DetermineBoundaries()
 	{
 		// still does not handle window resizing
